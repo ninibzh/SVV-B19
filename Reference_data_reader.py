6b018e27d6cdb1abd6c62c061389595e069a912f
gmmare <gmmare.me@gmail.com>
2020-03-05 16:30:16 +0100

    added reference data reading file

---
+++ Reference_data_reader.py
@@ -0,0 +1,7 @@
+import scipy.io
+import numpy as np
+mat = scipy.io.loadmat('Reference_data.mat') # this creates a dictionary with the data
+
+#print(mat.keys())
+Flight_Data = mat["flightdata"] #numpy.ndarray
+print(Flight_Data)
