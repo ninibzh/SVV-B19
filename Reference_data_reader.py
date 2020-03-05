e75f47e1c1f13347ba7f3bf8271a1ac94c494bd6
gmmare <gmmare.me@gmail.com>
2020-03-05 17:35:57 +0100

    added a part that converts .mat into a dictionary and allows the data to be read

--- Reference_data_reader.py
+++ Reference_data_reader.py
@@ -1,7 +1,60 @@
-import scipy.io
-import numpy as np
-mat = scipy.io.loadmat('Reference_data.mat') # this creates a dictionary with the data
+import scipy.io as spio
 
-#print(mat.keys())
-Flight_Data = mat["flightdata"] #numpy.ndarray
-print(Flight_Data)
+import numpy as np
+
+#reading the reference data
+'Reference_data.mat'
+
+#Functions for reading and converting the data from .mat to dictionaries.
+def loadmat(filename):
+    '''
+    this function should be called instead of direct spio.loadmat
+    as it cures the problem of not properly recovering python dictionaries
+    from mat files. It calls the function check keys to cure all entries
+    which are still mat-objects
+    '''
+    data = spio.loadmat(filename, struct_as_record=False, squeeze_me=True)
+    return _check_keys(data)
+
+def _check_keys(dict):
+    '''
+    checks if entries in dictionary are mat-objects. If yes
+    todict is called to change them to nested dictionaries
+    '''
+    for key in dict:
+        if isinstance(dict[key], spio.matlab.mio5_params.mat_struct):
+            dict[key] = _todict(dict[key])
+    return dict
+
+def _todict(matobj):
+    '''
+    A recursive function which constructs from matobjects nested dictionaries
+    '''
+    dict = {}
+    for strg in matobj._fieldnames:
+        elem = matobj.__dict__[strg]
+        if isinstance(elem, spio.matlab.mio5_params.mat_struct):
+            dict[strg] = _todict(elem)
+        else:
+
+            dict[strg] = elem
+    return dict
+
+#AC data
+reference_data = loadmat('Reference_data.mat')
+'''
+The mat file structure has 48 options. Each option is a parameter that is measured. Each option is split up into three options: units, data
+and description. When selecting data one should adhere this format:
+
+reference_data["flightdata"]["desired parameter to be read"]["data"]
+
+This returns an array with the data
+'''
+
+
+#getting the kets
+print(reference_data.keys())
+print(reference_data["flightdata"].keys())
+print()
+test_list = reference_data["flightdata"]["vane_AOA"]["units"]
+print(test_list)
